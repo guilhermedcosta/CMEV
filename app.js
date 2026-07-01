7710d8383b28725f1ef0f180cef68017d5d443bb
amor-del <amor@discente.ifpe.edu.br>
2026-07-01 18:50:29 -0300

    atualizado2.0

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const conn = require('./config/conn');
 const app = express();
+const pedidosRoutes = require('./routes/pedidos');
 
 // Configuração para receber dados de formulários e JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(pedidosRoutes);
 
 // Arquivos estáticos
 app.use(express.static('public'));
@@ -29,13 +31,13 @@ app.get('/', (req, res) => {
 });
 
 // Conexão com o banco e inicialização do servidor
-sequelize
-  .sync()
-  .then(() => {
+conn
+conn.sync({ force: true })
+.then(() => {
     app.listen(3000, () => {
       console.log('Servidor rodando em http://localhost:3000');
     });
   })
   .catch((err) => {
     console.error('Erro ao conectar com o banco de dados:', err);
-});
+  });
