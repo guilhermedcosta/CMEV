cb0771d009206380546b7206134f8783c3b0337c
amor-del <amor@discente.ifpe.edu.br>
2026-07-01 18:25:28 -0300

    atualizado

--- models/Pedido.js
+++ models/Pedido.js
@@ -2,18 +2,30 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/conn');
 
 const Pedido = sequelize.define('Pedido', {
-  setor: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  material: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  quantidade: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  }
+
+    cod_item: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+
+    setor: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    material: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    quantidade: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+
+}, {
+    timestamps: false
 });
 
 module.exports = Pedido;
