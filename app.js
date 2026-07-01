3e5ca5de88fe989ede2398ea6279593f80e78faf
Guilherme Costa <guilhermedcosta755@gmail.com>
2026-06-30 23:03:40 -0300

    up

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const express = require('express')
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const sequelize = require("./config/db");
+const Usuario = require("./models/Usuario");
+
+app.engine("handlebars", exphbs.engine({defaultLayout:false}));
+app.set("view engine","handlebars");
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use("/", require("./routes/usuarioRoutes"));
+
+sequelize.sync()
+.then(()=>{
+
+    app.listen(3000,()=>{
+        console.log("Servidor iniciado...");
+    });
+
+});
